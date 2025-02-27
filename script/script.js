74af6696c869f7c4c80c8469ec10c323e8ebed2c
Matias <26matmoran@gmail.com>
2025-02-27 10:39:31 -0600

    simplify logic to choose countries and put it inside play round func

--- script/script.js
+++ script/script.js
@@ -1,29 +1,28 @@
 let currentList = ALL_COUNTRIES_AND_TERRITORIES_ARRAY;
-
+let availableIndexesInList = currentList.length - 1;
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 
-let arrayOfNumbers0Through3 = [0, 1, 2, 3];
-let arrayOfAllChosenNumbers = [];
-let correctChoice = 0;
-for (let i = 0; i < 4; i++) {
-    let randomNumber = randomIntFromInterval(0, 254);
-    if (i === 0) {
-        correctChoice = randomNumber
-    }
-    let randomPosition = randomIntFromInterval(0, 3 - i);
-    arrayOfAllChosenNumbers[arrayOfNumbers0Through3[randomPosition]] = randomNumber;
-    arrayOfNumbers0Through3.splice(randomPosition, 1);
+function getIndexOfCorrectChoice() {
+    let randomIndex0Through3 = randomIntFromInterval(0, 3);
+    return randomIndex0Through3;
 }
-let choice1 = arrayOfAllChosenNumbers[0];
-let choice2 = arrayOfAllChosenNumbers[1];
-let choice3 = arrayOfAllChosenNumbers[2];
-let choice4 = arrayOfAllChosenNumbers[3];
-arrayOfNumbers0Through3 = [0, 1, 2, 3];
-arrayOfAllChosenNumbers = [];
 
-console.log(correctChoice)
-console.log(arrayOfAllChosenNumbers)
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+
+
+function playRound() {
+    let indexesOfAllChosenCountries = [];
+    for (let i = 0; i < 4; i++) {
+        let randomNumber = randomIntFromInterval(0, availableIndexesInList);
+        indexesOfAllChosenCountries[i] = randomNumber;
+        console.log(currentList[randomNumber])
+    }
+    let indexOfCorrectChoice = getIndexOfCorrectChoice();
+}
