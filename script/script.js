49c901923d03aee9fd118688f33d2da42387f18c
Matias <26matmoran@gmail.com>
2025-02-27 13:22:40 -0600

    add a next button that asks a new question.
    there was also a duplicate function to check if my choice was correct,
    so i deleted it.

--- script/script.js
+++ script/script.js
@@ -11,6 +11,9 @@ let displayedScore = document.querySelector(".current-score");
 let displayedHighScore = document.querySelector(".high-score")
 let userScore = 0;
 let highScore = 0;
+let nextRoundButton = document.querySelector(".next")
+let gameIsBeingPlayed = true;
+let indexOfCorrectChoice = ""
 
 
 function randomIntFromInterval(min, max) { // min and max included 
@@ -27,15 +30,6 @@ function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
 }
 
-function checkIfChoiceIsCorrect(string) {
-    if (string === currentList[indexOfCorrectChoice]) {
-        console.log("true")
-        return true;
-    } else {
-        console.log("false")
-        return false;
-    }
-}
 
 function checkIfChoiceIsCorrect(choice, correctChoice) {
     if (choice === correctChoice) {
@@ -43,11 +37,23 @@ function checkIfChoiceIsCorrect(choice, correctChoice) {
     } else {
         return false;
     }
-    console.log(result)
 }
 
+nextRoundButton.addEventListener("click", () => {
+    resetForNextRound();
+    playRound();
+})
+
+function resetForNextRound() {
+    allOptions.forEach(option => {
+        option.setAttribute("style", "border: none")
+    })
+    h1.textContent = "Guess the country";
+    nextRoundButton.setAttribute("style", "visibility: hidden;")
+}
 
 function playRound() {
+    gameIsBeingPlayed = true;
     let indexesOfAllChosenCountries = [];
     let userChoiceIsCorrect = "";
     for (let i = 0; i < 4; i++) {
@@ -55,7 +61,8 @@ function playRound() {
         indexesOfAllChosenCountries[i] = randomNumber;
         console.log(currentList[randomNumber])
     }
-    let indexOfCorrectChoice = getIndexOfCorrectChoice(indexesOfAllChosenCountries);
+    indexOfCorrectChoice = getIndexOfCorrectChoice(indexesOfAllChosenCountries);
+    console.log(indexOfCorrectChoice)
     flag.setAttribute("src", 
         "./images/flag-country-svg/" + 
         getKeyByValue(ALL_COUNTRIES_AND_TERRITORIES_OBJECT, currentList[indexOfCorrectChoice]).toLowerCase() 
@@ -66,29 +73,33 @@ function playRound() {
     option4.textContent = currentList[indexesOfAllChosenCountries[3]];
     allOptions.forEach(option => {
         option.addEventListener("click", () => {
-            optionText = option.querySelector("p");
-            userChoiceIsCorrect = checkIfChoiceIsCorrect(optionText.textContent, currentList[indexOfCorrectChoice]);
-            if (userChoiceIsCorrect === true) {
-                h1.textContent = "Correct!"
-                option.setAttribute("style", "border: 1px, solid, green")
-                userScore += 1;
-                if (userScore > highScore) {
-                    highScore = userScore;
-                    displayedScore.setAttribute("style", "color: rgb(225, 197, 100);")
-                    displayedHighScore.textContent = `High score: ${userScore}`;
-                }
-            } else {
-                h1.textContent = "Incorrect!"
-                option.setAttribute("style", "border: 1px, solid, red")
-                allOptions.forEach(option => {
-                    if (option.textContent === currentList[indexOfCorrectChoice]) {
-                        option.setAttribute("style", "border: 1px, solid, green")
-                        displayedScore.setAttribute("style", "color: rgb(0, 150, 137);")
+            if (gameIsBeingPlayed) {
+                optionText = option.querySelector("p");
+                userChoiceIsCorrect = checkIfChoiceIsCorrect(optionText.textContent, currentList[indexOfCorrectChoice]);
+                if (userChoiceIsCorrect === true) {
+                    h1.textContent = "Correct!"
+                    option.setAttribute("style", "border: 1px, solid, green")
+                    userScore += 1;
+                    if (userScore > highScore) {
+                        highScore = userScore;
+                        displayedScore.setAttribute("style", "color: rgb(225, 197, 100);")
+                        displayedHighScore.textContent = `High score: ${userScore}`;
                     }
-                })
-                userScore = 0;
+                } else {
+                    h1.textContent = "Incorrect!"
+                    option.setAttribute("style", "border: 1px, solid, red")
+                    allOptions.forEach(option => {
+                        if (option.textContent === currentList[indexOfCorrectChoice]) {
+                            option.setAttribute("style", "border: 1px, solid, green")
+                            displayedScore.setAttribute("style", "color: rgb(0, 150, 137);")
+                        }
+                    })
+                    userScore = 0;
+                }
+                gameIsBeingPlayed = false;
+                displayedScore.textContent = `Current score: ${userScore}`;
+                nextRoundButton.setAttribute("style", "visibility: visible;")
             }
-            displayedScore.textContent = `Current score: ${userScore}`;
         })
     })
 }
